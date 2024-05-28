489fc86efd086a673218b3fbfa4f3104710cbcda
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-28 22:27:55 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -2,10 +2,44 @@
 #include <windows.h>
 using namespace std;
 
+void pryam() {
+
+	int height, width;
+	cout << "¬вед≥ть висоту: ";
+	cin >> height;
+	cout << "¬вед≥ть ширину: ";
+	cin >> width;
+	for (int i = 0; i < width; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 0; i < height - 1; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0 || j == width - 1) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+
+		cout << endl;
+	}
+	for (int i = 0; i < width; i++)
+	{
+		cout << "*";
+	}
+
+}
+
 
 int main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
+	pryam();
 
 	return 0;
 }
