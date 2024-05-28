3b96ca97bc6fffe496823ca026b16e36ab7f27cb
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-28 22:36:16 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
-#include <windows.h>
+#include <Windows.h>
+
 using namespace std;
 
+void number() {
 
+	int number_1, number_2;
+	cout << "¬вед≥ть число 1: ";
+	cin >> number_1;
+	cout << "¬вед≥ть число 2: ";
+	cin >> number_2;
+	if (number_1 > number_2) {
+		cout << "„исло " << number_1 << " б≥льше за число  " << number_2 << endl;
+	}
+	else {
+		cout << "„исло " << number_2 << " б≥льше за число  " << number_1 << endl;
+	}
+	cout << endl;
+
+
+}
 int main() {
-	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	number();
 
 	return 0;
 }
