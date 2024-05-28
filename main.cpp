1037fa490a96c7f4b71f766cf6081e0f88ef543b
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-28 22:33:46 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 #include <windows.h>
 using namespace std;
 
+void simple_number() {
+	int n;
+	cout << "Введіть число: ";
+	cin >> n;
+	for (int i = 2; i <= n / 2; i++) {
+		if (n % i == 0) {
+			cout << "Число не є простим" << endl;
+			return;
+		}
+	}
+	cout << "Число є простим" << endl;
+
+}
 
 int main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
-
+	simple_number();
 	return 0;
 }
