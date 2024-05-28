64a41c7f6cebfc72e575f1b42986519ed33616ae
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-28 22:37:24 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <windows.h>
+
 using namespace std;
+void number() {
+
+	int number;
+	cout << "¬вед≥ть число: ";
+	cin >> number;
+	if (number > 0) {
+		cout << "„исло " << number << " Ї додатн≥м  " << endl;
+	}
+	else {
+		cout << "Error!!!" << endl;
+	}
+	cout << endl;
 
 
+}
 int main() {
-	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	number();
 
 	return 0;
 }
